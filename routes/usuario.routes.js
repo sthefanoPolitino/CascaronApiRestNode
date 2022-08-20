eb85c05d66577cedaf089fff89961b2316186d85
sthefano <spolitino@popapp.io>
2022-08-20 20:57:44 +0200

    agregando carpetas de services,test y agregando comentarios

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,11 +1,18 @@
-const { Router } = require('express')
-const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios.controller')
-const {loguearRequest}=require('../middlewares/utils')
+const { Router } = require("express");
+const {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch,
+} = require("../controllers/usuarios.controller");
+const { loguearRequest } = require("../middlewares/utils");
 
-const router = Router()
-router.get('',[loguearRequest], usuariosGet)
-router.put('/:id',[loguearRequest], usuariosPut) //forma de pasar parametros por la url
-router.post('', [loguearRequest],usuariosPost)
-router.delete('', [loguearRequest],usuariosDelete)
-router.patch('', [loguearRequest],usuariosPatch)
+const router = Router();
+//se puede validar atributos de body, query, params especificamente, ejemplo query("expresion", "La expresion es requerida").not().isEmpty()
+router.get("", [loguearRequest], usuariosGet);
+router.put("/:id", [loguearRequest], usuariosPut); //forma de pasar parametros por la url
+router.post("", [loguearRequest], usuariosPost);
+router.delete("", [loguearRequest], usuariosDelete);
+router.patch("", [loguearRequest], usuariosPatch);
 module.exports = router;
