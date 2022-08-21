724a3b8c3cc3dc8df44aeba6007030fe7bff9619
sthefano <spolitino@popapp.io>
2022-08-22 00:00:27 +0200

    mejorando middleware que loguea las request entrantes

--- middlewares/utils.js
+++ middlewares/utils.js
@@ -1,6 +1,14 @@
- /**middleware personalizado para loguear cada vez que entra una request */
- const loguearRequest=(req, res, next) => {
-    console.log(`Ingresa request al path ${JSON.stringify(req.route?.path)} desde IP ${req.ip} con headers ${JSON.stringify(req.headers)}, tipo: ${req.method} y body: ${JSON.stringify(req.body)}`);
-    return next();
-}
-module.exports={loguearRequest}
+const { request } = require("express");
+
+/**middleware personalizado para loguear cada vez que entra una request */
+const loguearRequest = (req = request, res, next) => {
+  console.log(
+    `Ingresa request al path ${JSON.stringify(
+      req.baseUrl + req.path
+    )} desde IP ${req.ip} con headers ${JSON.stringify(req.headers)}, tipo: ${
+      req.method
+    } y body: ${JSON.stringify(req.body)}`
+  );
+  return next();
+};
+module.exports = { loguearRequest };
